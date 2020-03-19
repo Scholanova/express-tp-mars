7871b3eba9ed28bed5ecd6b3ea95b4ceeed646f4
FrancoisGodin <francois.h.godin@gmail.com>
2020-03-19 14:47:34 +0100

    ADD substract function on CalculusService + test

--- test/services/calculusServiceTest.js
+++ test/services/calculusServiceTest.js
@@ -1,9 +1,10 @@
 const { expect } = require('../testHelper')
 
 const calculusService = require('../../lib/services/calculusService')
-const { NotEvenResultError } = require('../../lib/errors')
+const { NotEvenResultError, NegativeResultError } = require('../../lib/errors')
 
 describe('calculusService', () => {
+
   describe('sum', () => {
 
     let operand1
@@ -23,8 +24,8 @@ describe('calculusService', () => {
 
       it('should return the sum', () => {
         // then
-        const expectedSum = operand1 + operand2
-        return expect(sumPromise).to.eventually.equal(expectedSum)
+        const expectedResult = operand1 + operand2
+        return expect(sumPromise).to.eventually.equal(expectedResult)
       })
     })
 
@@ -39,9 +40,50 @@ describe('calculusService', () => {
         sumPromise = calculusService.sum(operand1, operand2) 
       })
 
-      it('should throw an not even error', () => {
+      it('should throw a not even error', () => {
         //then
         return expect(sumPromise).to.eventually.be.rejectedWith(NotEvenResultError)
+      })
+    })
+  })
+
+  describe('substract', () => {
+    let operand1
+    let operand2
+    let substractPromise
+  
+    context('when the two operands substract to a positive number', () => {
+      
+      beforeEach(() => {
+        //given
+        operand1 = 2
+        operand2 = 1
+
+        //when 
+        substractPromise = calculusService.substract(operand1, operand2)
+      })
+
+      it('should return the substraction', () => {
+        //then
+        const expectedResult = operand1 - operand2
+        return expect(substractPromise).to.eventually.equal(expectedResult)
+      })
+    })
+
+    context('when the two operands substract to a negative result', () => {
+       
+      beforeEach(() =>{
+        //given 
+        operand1 = 4
+        operand2 = 7
+
+        //when
+        substractPromise = calculusService.substract(operand1, operand2)
+      })
+
+      it('should throw a negative error', () => {
+        //then 
+        return expect(substractPromise).to.eventually.be.rejectedWith(NegativeResultError)
       })
     })
   })
