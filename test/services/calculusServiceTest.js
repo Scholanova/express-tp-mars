c1e3e62d685a8942c6a058f7b5b7d5f39c2a6f81
Daouda Fofana <fofanadaouda93230@gmail.com>
2020-03-19 14:45:42 +0100

    substract function

--- test/services/calculusServiceTest.js
+++ test/services/calculusServiceTest.js
@@ -1,28 +1,84 @@
-const { expect, sinon } = require('../testHelper')
+const { expect } = require('../testHelper')
 
 const calculusService = require('../../lib/services/calculusService')
+const { NotEvenResultError,NotPositiveResultError } = require('../../lib/errors')
 
 describe('calculusService', () => {
+  
+  describe('sum', () => {
 
-  let operand1
-  let operand2
-  let result
+    let operand1
+    let operand2
+    let sumPromise
 
-  context('when the two operands sum to a even number', () => {
+    context('when the two operands sum to a even number', () => {
 
-    beforeEach(async () => {
-      // given
-      operand1 = 1
-      operand2 = 3
+      beforeEach(() => {
+        // given
+        operand1 = 1
+        operand2 = 3
 
-      // when
-      result = await calculusService.sum(operand1, operand2)
+        // when
+        sumPromise = calculusService.sum(operand1, operand2)
+      })
+
+      it('should return the sum', () => {
+        // then
+        const expectedSum = operand1 + operand2
+        return expect(sumPromise).to.eventually.equal(expectedSum)
+      })
     })
 
-    it('should return the sum', () => {
-      // then
-      const expectedSum = operand1 + operand2
-      expect(result).to.be.equal(expectedSum)
+    context('when the two operands sum to an odd number', () => {
+
+      beforeEach(() => {
+        // given
+        operand1 = 1
+        operand2 = 2
+
+        // when
+        sumPromise = calculusService.sum(operand1, operand2)
+      })
+
+      it('should throw an not even error', () => {
+        // then
+        return expect(sumPromise).to.eventually.be.rejectedWith(NotEvenResultError)
+      })
+    })
+
+    context('when the two operands substract to a positive number', () => {
+
+      beforeEach(() => {
+        // given
+        operand1 = 3
+        operand2 = 1
+  
+        // when
+        sumPromise = calculusService.min(operand1, operand2)
+      })
+  
+      it('should return the substract', () => {
+        // then
+        const expectedSum = operand1 - operand2
+        return expect(sumPromise).to.eventually.equal(expectedSum)
+      })
+    })
+
+    context('when the two operands sum to a negative number', () => {
+
+      beforeEach(() => {
+        // given
+        operand1 = 1
+        operand2 = 2
+
+        // when
+        sumPromise = calculusService.min(operand1, operand2)
+      })
+
+      it('should throw an not positive error', () => {
+        // then
+        return expect(sumPromise).to.eventually.be.rejectedWith(NotPositiveResultError)
+      })
     })
   })
 })
