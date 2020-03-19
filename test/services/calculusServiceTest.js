5515cf9f79589c73f91c7bfd71dd157829ba882a
nicolas gomes <Gomes.Nicolas@outlook.fr>
2020-03-19 12:08:22 +0100

    Sub+Test

--- test/services/calculusServiceTest.js
+++ test/services/calculusServiceTest.js
@@ -1,45 +1,89 @@
-const { expect, sinon } = require('../testHelper')
+const { expect } = require('../testHelper')
 
 const calculusService = require('../../lib/services/calculusService')
-const { NotEvenResultError } = require('../../lib/errors');
+const { NotEvenResultError, NegativeResultError } = require('../../lib/errors');
 
 describe('calculusService', () => {
 
-  let operand1
-  let operand2
-  let result
+  describe('sum', () => {
+    let operand1
+    let operand2
+    let sumPromise
 
-  context('when the two operands sum to a even number', () => {
+    context('when the two operands sum to a even number', () => {
 
-    beforeEach(async () => {
-      // given
-      operand1 = 1
-      operand2 = 3
+      beforeEach(() => {
+        // given
+        operand1 = 1
+        operand2 = 3
 
-      // when
-      result = await calculusService.sum(operand1, operand2)
+        // when
+        sumPromise = calculusService.sum(operand1, operand2)
+      })
+
+      it('should return the sum', () => {
+        // then
+        const expectedSum = operand1 + operand2
+        return expect(sumPromise).to.eventually.equal(expectedSum)
+      })
     })
 
-    it('should return the sum', () => {
-      // then
-      const expectedSum = operand1 + operand2
-      expect(result).to.be.equal(expectedSum)
+    context('when the two operands sum to a odd number', () => {
+
+      beforeEach(() => {
+        // given
+        operand1 = 1
+        operand2 = 2
+
+        //when
+        sumPromise = calculusService.sum(operand1, operand2)
+      })
+
+      it('should throw NotEventResultError', () => {
+        // when
+        return expect(sumPromise).to.eventually.be.rejectedWith(NotEvenResultError)
+      })
     })
   })
 
-  context('when the two operands sum to a odd number', () => {
+  describe('sum', () => {
+    let operand1
+    let operand2
+    let sumPromise
 
-    beforeEach(() => {
-      // given
-      operand1 = 1
-      operand2 = 2
-      result = operand1 + operand2
+    context('when the two operands sum equal op1 - op2', () => {
+
+      beforeEach(() => {
+        // given
+        operand1 = 4
+        operand2 = 2
+
+        // when
+        subPromise = calculusService.sub(operand1, operand2)
+      })
+
+      it('should return the sum', () => {
+        // then
+        const expectedSum = operand1 - operand2
+        return expect(subPromise).to.eventually.equal(expectedSum)
+      })
     })
 
-    it('should throw NotEventResultError', () => {
-      // when
-      expect(calculusService.sum(operand1, operand2)).to.eventually.throw(new NotEvenResultError(result))
-    })
+    context('when the two operands sum equal negative', () => {
 
+      beforeEach(() => {
+        // given
+        operand1 = 2
+        operand2 = 4
+
+        // when
+        subPromise = calculusService.sub(operand1, operand2)
+      })
+
+      it('should throw an error', () => {
+        // then
+        return expect(subPromise).to.eventually.be.rejectedWith(NegativeResultError)
+      })
+    })
   })
 })
