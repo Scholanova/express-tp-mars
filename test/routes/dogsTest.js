42c71ba5a439a877b4d3a9b67e061db922f67dc2
Namelistone <namelistone@gmail.com>
2020-03-19 16:17:21 +0100

    show + test

--- test/routes/dogsTest.js
+++ test/routes/dogsTest.js
@@ -3,7 +3,7 @@ const app = require('../../lib/app')
 const dogRepository = require('../../lib/repositories/dogRepository')
 const models = require('../../lib/models')
 const Dog = models.Dog
-
+const { DogNotFoundError } = require('../../lib/errors')
 describe('dogRoutes', () => {
 
   describe('list', () => {
@@ -78,5 +78,58 @@ describe('dogRoutes', () => {
     //     expect(resultValues).to.deep.equal([dogValue])
     //   })
     // })
+  })
+  describe('id', () => {
+    let dogId
+    let response
+
+    beforeEach(() => {
+      sinon.stub(dogRepository, 'get')
+    })
+
+    context('when the id dogs not in the repository', () => {
+      dogId = 1
+      beforeEach(async () => {
+        // given
+        dogRepository.get.rejects(new DogNotFoundError())
+
+        // when
+        response = await request(app).get(`/dogs/${dogId}`)
+      })
+
+      it('should failed with a status 404', () => {
+        // then
+        expect(response).to.have.status(404)
+      })
+
+      it('should return an empty list message', () => {
+        // then
+        expect(response).to.be.html
+        expect(response.text).to.contain('This page does not exist')
+      })
+    })
+    context('when the id dogs is in the repository', () => {
+      dogId = 2
+      beforeEach(async () => {
+        // given
+        const dog = new Dog({ name: 'Rex', age: 12 })
+        dogRepository.get.resolves(dog)
+
+        // when
+        response = await request(app).get(`/dogs/${dogId}`)
+      })
+
+      it('should success with a status 200', () => {
+        // then
+        expect(response).to.have.status(200)
+      })
+
+      it('should return an empty list message', () => {
+        // then
+        expect(response).to.be.html
+        expect(response.text).to.contain('Rex - 12')
+      })
+    })
+
   })
 })
