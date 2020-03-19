ca65fa17b0e90b9b37427bfde164085b1ed9e526
LivioChan <ctlivio@yahoo.fr>
2020-03-19 11:19:37 +0100

    update

--- test/services/calculusServiceTest.js
+++ test/services/calculusServiceTest.js
@@ -24,5 +24,39 @@ describe('calculusService', () => {
       const expectedSum = operand1 + operand2
       expect(result).to.be.equal(expectedSum)
     })
+  }),
+  context('when the two operands sum to a odd number', () => {
+
+    beforeEach(async () => {
+      // given
+      operand1 = 1
+      operand2 = 3
+
+      // when
+      result = await calculusService.sum(operand1, operand2)
+    })
+
+    it('should return the sum', () => {
+      // then
+      const expectedSum = operand1 + operand2
+      expect(result).to.be.equal(expectedSum)
+    })
+  }),
+  context('when the two operands sub to a number', () => {
+
+    beforeEach(async () => {
+      // given
+      operand1 = 9
+      operand2 = 4
+
+      // when
+      result = await calculusService.sub(operand1, operand2)
+    })
+
+    it('should return the subtract', () => {
+      // then
+      const expectedSub = operand1 - operand2
+      expect(result).to.be.equal(expectedSub)
+    })
   })
 })
