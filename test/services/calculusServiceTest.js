b31e22edb396a6d0cf7a8cb936bf8436dd31f7ca
Antoine Boileau <antoine@deliver.ee>
2020-03-19 14:01:50 +0100

    ADD subtraction function on calculus service

--- test/services/calculusServiceTest.js
+++ test/services/calculusServiceTest.js
@@ -1,10 +1,10 @@
 const { expect } = require('../testHelper')
 
 const calculusService = require('../../lib/services/calculusService')
-const { NotEvenResultError } = require('../../lib/errors')
+const { NegativeResultError, NotEvenResultError } = require('../../lib/errors')
 
 describe('calculusService', () => {
-  
+
   describe('sum', () => {
 
     let operand1
@@ -40,9 +40,51 @@ describe('calculusService', () => {
         sumPromise = calculusService.sum(operand1, operand2)
       })
 
-      it('should throw an not even error', () => {
+      it('should throw a not even error', () => {
         // then
         return expect(sumPromise).to.eventually.be.rejectedWith(NotEvenResultError)
+      })
+    })
+  })
+
+  describe('subtract', () => {
+
+    let operand1
+    let operand2
+    let subtractPromise
+
+    context('when the two operands subtract to a positive number', () => {
+
+      beforeEach(() => {
+        // given
+        operand1 = 5
+        operand2 = 3
+
+        // when
+        subtractPromise = calculusService.subtract(operand1, operand2)
+      })
+
+      it('should return the subtraction', () => {
+        // then
+        const expectedResult = operand1 - operand2
+        return expect(subtractPromise).to.eventually.equal(expectedResult)
+      })
+    })
+
+    context('when the two operands subtract to a negative number', () => {
+
+      beforeEach(() => {
+        // given
+        operand1 = 1
+        operand2 = 2
+
+        // when
+        subtractPromise = calculusService.subtract(operand1, operand2)
+      })
+
+      it('should throw a not even error', () => {
+        // then
+        return expect(subtractPromise).to.eventually.be.rejectedWith(NegativeResultError)
       })
     })
   })
