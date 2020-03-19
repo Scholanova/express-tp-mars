6d7de991215175d76ef441e18b78ca964e08614d
David <dvotrgi@gmail.com>
2020-03-19 14:17:34 +0100

    adds substract tests

--- test/services/calculusServiceTest.js
+++ test/services/calculusServiceTest.js
@@ -1,4 +1,5 @@
 const { expect, sinon } = require('../testHelper')
+const { NotEvenResultError,  MustBePositiveError} = require('../../lib/errors')
 
 const calculusService = require('../../lib/services/calculusService')
 
@@ -22,7 +23,57 @@ describe('calculusService', () => {
     it('should return the sum', () => {
       // then
       const expectedSum = operand1 + operand2
-      expect(result).to.be.equal(expectedSum)
+      return expect(result).to.be.equal(expectedSum)
+    })
+  })
+
+  context('when the two operands sum to an odd number', () => {
+
+    beforeEach(async () => {
+      // given
+      operand1 = 1
+      operand2 = 2
+
+    })
+
+    it('should return the sum', () => {
+      // when
+      expect(calculusService.sum(operand1, operand2)).to.eventually.throw(new NotEvenResultError(result))
+    })
+  })
+
+  context('when the two operands substract to a positive number', () => {
+
+    beforeEach(async () => {
+      // given
+      operand1 = 3
+      operand2 = 2
+
+      // when
+      result = await calculusService.substract(operand1, operand2)
+    })
+
+    it('should return the substract', () => {
+      // then
+      const expectedSubstract = operand1 - operand2
+      return expect(result).to.be.equal(expectedSubstract)
+    })
+  })
+
+  context('when the two operands substract to a negative number', () => {
+
+    beforeEach(async () => {
+      // given
+      operand1 = 1
+      operand2 = 2
+
+      // when
+      promise = calculusService.substract(operand1, operand2)
+    })
+
+    it('should return the substract', () => {
+      // then
+      expect(promise).to.eventually.throw(new MustBePositiveError(result))
     })
   })
 })
