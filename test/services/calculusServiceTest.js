bfdfa9d35ee87e3c4ba13a4e8cdd7cfe56f952ab
Florian <florianbematol@gmail.com>
2020-03-19 11:42:01 +0100

    CalculService add substract methods + test

--- test/services/calculusServiceTest.js
+++ test/services/calculusServiceTest.js
@@ -1,7 +1,7 @@
 const { expect, sinon } = require('../testHelper')
 
 const calculusService = require('../../lib/services/calculusService')
-const { NotEvenResultError } = require('../../lib/errors');
+const { NotEvenResultError, NegativeResultError } = require('../../lib/errors');
 
 describe('calculusService', () => {
 
@@ -11,19 +11,19 @@ describe('calculusService', () => {
 
   context('when the two operands sum to a even number', () => {
 
-    beforeEach(async () => {
+    beforeEach(() => {
       // given
       operand1 = 1
       operand2 = 3
 
       // when
-      result = await calculusService.sum(operand1, operand2)
+      result = calculusService.sum(operand1, operand2)
     })
 
     it('should return the sum', () => {
       // then
       const expectedSum = operand1 + operand2
-      expect(result).to.be.equal(expectedSum)
+      return expect(result).to.eventually.equal(expectedSum)
     })
   })
 
@@ -37,8 +37,59 @@ describe('calculusService', () => {
     })
 
     it('should throw NotEventResultError', () => {
+      // when & then
+      return expect(calculusService.sum(operand1, operand2)).to.eventually.be.rejectedWith(NotEvenResultError)
+    })
+
+  })
+
+  context('when the two operands substract to a even number', () => {
+
+    beforeEach(() => {
+      // given
+      operand1 = 3
+      operand2 = 1
+      
       // when
-      expect(calculusService.sum(operand1, operand2)).to.eventually.throw(new NotEvenResultError(result))
+      result = calculusService.substract(operand1, operand2)
+    })
+
+    it('should return the sum', () => {
+      // then
+      const expectedSum = operand1 - operand2
+      return expect(result).to.eventually.equal(expectedSum)
+    })
+
+  })
+
+  context('when two operands substract to a positive number', () => {
+
+    beforeEach(() => {
+      // given
+      operand1 = 4
+      operand2 = 1
+      result = operand1 - operand2
+    })
+
+    it('should throw NotEventResultError', () => {
+      // when & then
+      return expect(calculusService.substract(operand1, operand2)).to.eventually.equal(result)
+    })
+
+  })
+
+  context('when two operands substract to a negative number', () => {
+
+    beforeEach(() => {
+      // given
+      operand1 = 1
+      operand2 = 4
+      result = operand1 - operand2
+    })
+
+    it('should throw NotEventResultError', () => {
+      // when & then
+      return expect(calculusService.substract(operand1, operand2)).to.eventually.be.rejectedWith(NegativeResultError)
     })
 
   })
