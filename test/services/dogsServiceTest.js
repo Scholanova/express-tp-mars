5c4b471f33a5b16b5291e0f1a902abc432da0ce8
Namelistone <namelistone@gmail.com>
2020-03-20 12:27:46 +0100

    ajout de test sur dogsService

--- test/services/dogsServiceTest.js
+++ test/services/dogsServiceTest.js
@@ -1,51 +1,107 @@
-const { expect } = require('../testHelper')
-
+const { expect, sinon } = require('../testHelper')
+const dogRepository = require('../../lib/repositories/dogRepository')
 const dogsService = require('../../lib/services/dogsService')
-const { NameCantBeEmptyError,AgeCantBeEmptyError,AgeCantbeNegativeError } = require('../../lib/errors')
-
+const { NameCantBeEmptyError,AgeCantBeEmptyError,AgeCantbeNegativeError,AgeCantNotBeANumberError } = require('../../lib/errors')
+const models = require('../../lib/models')
+const Dog = models.Dog
 describe('dogService', () => {
 
   describe('create', () => {
     let dogData
+    let dogPromise
+
     beforeEach(() => {
         sinon.stub(dogRepository, 'create')
       })
 
     
     context('when the dog data is valid', () => {
+        let dog
 
       beforeEach(() => {
         // given
         dogData = { name: 'Rex', age: 12 }
+        dog = new Dog({id : 1 , name : 'Rex', age: 12})
+        dogRepository.create.resolves(dog)
         // when
         dogPromise= dogsService.create(dogData)
+
+
       })
 
-      it('should call the dog Repository with the creation data', () => {
+      it('should call the dog Repository with the creation data', async () => {
         // then
+        await dogPromise
         expect(dogRepository.create).to.have.been.calledWith(dogData)
       })
       it('should resolve with the created dog from repository', () => {
         // then
-        const expectedDog = { name: 'Rex', age: 12 }
-        return expect(dogPromise).to.eventually.equal(expectedDog)
+        return expect(dogPromise).to.eventually.equal(dog)
       })
     })
 
-    context('when the dog data is missing a name', () => {
+    context('when the dog name is missing', () => {
 
       beforeEach(() => {
         // given
-
+        dogData = { name: '', age: 12 }
         // when
+        dogPromise= dogsService.create(dogData)
       })
 
-      it('should not call the dog Repository', () => {
+      it('should not call the dog Repository', async () => {
         // then
+        await dogPromise.catch(()=>{})
+    
+        expect(dogRepository.create).to.not.have.been.called
+        
       })
-      it('should reject with a missing parameter error', () => {
+      it('should reject with a missing NameCantBeEmptyError error', () => {
         // then
+        return expect(dogPromise).to.eventually.be.rejectedWith(NameCantBeEmptyError)
       })
     })
+    context('when the dog age is missing', () => {
+
+        beforeEach(() => {
+          // given
+          dogData = { name: 'Rex' }
+          // when
+          dogPromise= dogsService.create(dogData)
+        })
+  
+        it('should not call the dog Repository', async () => {
+          // then
+          await dogPromise.catch(()=>{})
+      
+          expect(dogRepository.create).to.not.have.been.called
+          
+        })
+        it('should reject with a missing AgeCantBeEmptyError error', () => {
+          // then
+          return expect(dogPromise).to.eventually.be.rejectedWith(AgeCantBeEmptyError)
+        })
+      })
+      context('when the dog age is not a number', () => {
+
+        beforeEach(() => {
+          // given
+          dogData = { name: 'Rex' }
+          // when
+          dogPromise= dogsService.create(dogData)
+        })
+  
+        it('should not call the dog Repository', async () => {
+          // then
+          await dogPromise.catch(()=>{})
+      
+          expect(dogRepository.create).to.not.have.been.called
+          
+        })
+        it('should reject with a missing AgeCantNotBeANumberError error', () => {
+          // then
+          return expect(dogPromise).to.eventually.be.rejectedWith(AgeCantNotBeANumberError)
+        })
+      })
   })
 })
