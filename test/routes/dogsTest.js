543f2709fe83f636e95b0ed255f67284a9f50bd2
Cyril VUILLEMIN <cyril.vuillemin@hotmail.fr>
2020-03-20 16:03:22 +0100

    add creating form + test + notification erros in form

--- test/routes/dogsTest.js
+++ test/routes/dogsTest.js
@@ -1,7 +1,9 @@
 const { expect, request, sinon } = require('../testHelper')
 const app = require('../../lib/app')
 const dogRepository = require('../../lib/repositories/dogRepository')
+const dogsService = require('../../lib/services/dogsService')
 const models = require('../../lib/models')
+const { NameMustBeFilled, AgeMustBeFilled, AgeMustBePositive } = require('../../lib/errors')
 const Dog = models.Dog
 
 describe('dogRoutes', () => {
@@ -121,6 +123,76 @@ describe('dogRoutes', () => {
         // then
         expect(response).to.be.html
         expect(response.text).to.contain('Rex - 12')
+      })
+    })
+  })
+
+  describe('post Create', () => {
+
+    let response
+
+    beforeEach(() => {
+      sinon.stub(dogsService, 'create')
+    })
+
+    context('when post a valid dog', () => {
+
+      beforeEach(async () => {
+        // when
+        dogs = new Dog({name: 'jean', age: 6})
+        dogsService.create.resolves(dogs)
+        response = await request(app).post('/dogs/create').type('form').send({ '_method': 'post', 'name': 'jean', 'age': '6' })
+      })
+
+      it('should return status 404', () => {
+        // then
+        expect(response).to.be.html
+        expect(response.text).to.contain('jean - 6')
+      })
+    })
+
+    context('when post an dog without name', () => {
+      beforeEach(async () => {
+        // when
+        dogs = new Dog({name: '', age: 6})
+        dogsService.create.rejects(new NameMustBeFilled())
+        response = await request(app).post('/dogs/create').type('form').send({ '_method': 'post', 'name': '', 'age': '6' })
+      })
+
+      it('should return an notification in the form', () => {
+        // then
+        expect(response).to.be.html
+        expect(response.text).to.contain('Name must be filled')
+      })
+    })
+
+    context('when post an dog without age', () => {
+      beforeEach(async () => {
+        // when
+        dogs = new Dog({name: '', age: 6})
+        dogsService.create.rejects(new AgeMustBeFilled())
+        response = await request(app).post('/dogs/create').type('form').send({ '_method': 'post', 'name': 'jean', 'age': '' })
+      })
+
+      it('should return an notification in the form', () => {
+        // then
+        expect(response).to.be.html
+        expect(response.text).to.contain('Age must be filled')
+      })
+    })
+
+    context('when post an dog with age < 0 ', () => {
+      beforeEach(async () => {
+        // when
+        dogs = new Dog({name: '', age: 6})
+        dogsService.create.rejects(new AgeMustBePositive())
+        response = await request(app).post('/dogs/create').type('form').send({ '_method': 'post', 'name': 'jean', 'age': '0' })
+      })
+
+      it('should return an notification in the form', () => {
+        // then
+        expect(response).to.be.html
+        expect(response.text).to.contain('Age must be positive')
       })
     })
   })
